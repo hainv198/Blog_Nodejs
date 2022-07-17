fc2333fc78775a82d39a7e73f8a83d425024da85
Your Name <you@example.com>
2022-07-17 22:18:18 +0700

    nodejs

---
+++ Remame.md
@@ -0,0 +1,15 @@
+# nodemone : lắng nghe sự thay đổi của file trong code rồi F5 lại ứng dụng k phải thoats như trc nữa
+
+# Cú pháp: npm i nodemon --save-dev
+
+# morgan npm => cu phap npm i morgan
+
+# handlebars đang cài đặt là bản v5.1.0 => npm i express-handlebars@5.1.0
+
+# app.set('views', path.join(\_\_dirname,'resources','views'))
+
+# cấu hình đường path ảnh Static File
+
+# app.use(express.static(path.join(\_\_dirname,'Public')))
+
+# SCSS
---
+++ src/index.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const express = require('express')
+const exphbs  = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname,'Public')))
+
+const morgan = require('morgan')
+
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resources','views'))
+// console.log('path' ,path.join(__dirname,'resources','views') )
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/new', (req, res) => {
+    res.render('news')
+})
+
+app.listen(port, () => {
+    console.log(`server dang chay o cong ${port}`);
+})
